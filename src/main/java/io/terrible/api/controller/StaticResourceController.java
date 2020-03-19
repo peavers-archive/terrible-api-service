12fbdf979eef3edd576d9a852084331406ffaf73
Chris Turner <chris@forloop.space>
2020-03-19 19:34:04 +0000

    feature: Handle request for static assets

---
+++ src/main/java/io/terrible/api/controller/StaticResourceController.java
@@ -0,0 +1,35 @@
+package io.terrible.api.controller;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+/** @author Chris Turner (chris@forloop.space) */
+@RestController
+public class StaticResourceController {
+
+  @GetMapping(value = "static-resource/image", produces = MediaType.IMAGE_JPEG_VALUE)
+  public Resource image(@RequestParam final String path) throws MalformedURLException {
+
+    final File file = new File(path);
+
+    return file.canRead()
+        ? new FileSystemResource(file)
+        : new UrlResource("https://via.placeholder.com/150");
+  }
+
+  @GetMapping(value = "static-resource/video", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+  public Resource video(@RequestParam final String path) {
+
+    final File file = new File(path);
+
+    return file.canRead() ? new FileSystemResource(file) : null;
+  }
+}

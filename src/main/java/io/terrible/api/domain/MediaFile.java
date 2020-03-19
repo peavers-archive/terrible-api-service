3bc94ecd5a78613abbd6cacc36ea67a644f4e30b
Chris Turner <chris@forloop.space>
2020-03-19 19:01:22 +0000

    clean: code format

--- src/main/java/io/terrible/api/domain/MediaFile.java
+++ src/main/java/io/terrible/api/domain/MediaFile.java
@@ -2,16 +2,13 @@
 package io.terrible.api.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 /** @author Chris Turner (chris@forloop.space) */
 @Data
--- src/main/java/io/terrible/api/domain/ThumbnailList.java
+++ src/main/java/io/terrible/api/domain/ThumbnailList.java
@@ -1,13 +1,12 @@
+/* Licensed under Apache-2.0 */
 package io.terrible.api.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.nio.file.Path;
-import java.util.ArrayList;
 
 /** @author Chris Turner (chris@forloop.space) */
 @Data
